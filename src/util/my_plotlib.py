1242aa89cd9757f054832dd7f22d952a270b71e3
jimkon <kontzedakis_93@hotmail.com>
2017-12-19 17:59:37 +0200

    add seperators

--- src/util/my_plotlib.py
+++ src/util/my_plotlib.py
@@ -18,8 +18,7 @@ class Line:
             plt.xlabel("x")
 
         line = self
-        max_y = np.amax(line.y)
-        min_y = np.amin(line.y)
+        max_y, min_y = self.y_range()
 
         plt.plot(line.x, line.y, line.color, linewidth=line.width)
         plt.text(0.05 * len(line.y), k * 0.1 * (max_y - min_y),
@@ -27,6 +26,9 @@ class Line:
 
         if fig is None:
             plt.show()
+
+    def y_range(self):
+        return np.amin(self.y), np.amax(self.y)
 
 
 class Function(Line):
@@ -36,13 +38,25 @@ class Function(Line):
         super().__init__(x, y, line_width, line_color, text)
 
 
-def plot_lines(lines):
+def plot_lines(lines, seps=None):
     fig = plt.figure()
     plt.grid(True)
     plt.ylabel("y")
     plt.xlabel("x")
+    max_y = []
+    min_y = []
     count = 0
     for line in lines:
         count += 1
         line.plot(fig=fig, k=count)
+        temp = line.y_range()
+        min_y.append(temp[0])
+        max_y.append(temp[1])
+
+    min_y = np.amin(min_y)
+    max_y = np.amax(max_y)
+
+    for s in seps:
+        plt.plot([s - 0.001, s + 0.001], [min_y, max_y], 'r', linewidth=0.5)
+        print(s)
     plt.show()
