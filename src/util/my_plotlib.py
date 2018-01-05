898e87faaba86b88b7be7f338b6cfef8ba091a2f
jimkon <kontzedakis_93@hotmail.com>
2018-01-05 20:38:57 +0200

    minor change

--- src/util/my_plotlib.py
+++ src/util/my_plotlib.py
@@ -1,41 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from random import randint
 
 
 class Line:
-    def __init__(self, x, y, line_width=1, line_color=None, text='', style='-'):
+    def __init__(self, x, y, line_width=1, line_color='black', text='', style='-'):
         self.x = x
         self.y = y
         self.width = line_width
-
-        if line_color is None:
-
-            random_hex = str(hex(randint(0x111111, 0xcccccc)))
-            self.color = '#' + random_hex[2:]
-        else:
-            self.color = line_color
-
+        self.color = line_color
         self.text = text
         self.style = style
 
-    def plot(self, fig=None, text_y=1, log=False, title=''):
+    def plot(self, fig=None, k=2):
         if fig is None:
             plt.figure()
             plt.grid(True)
-            plt.ylabel('y')
-            plt.xlabel('x')
-
-        plt.title(title)
+            plt.ylabel("y")
+            plt.xlabel("x")
 
         max_y, min_y = self.y_range()
 
-        plt.plot(self.x,
-                 self.y,
-                 self.color,
-                 linewidth=self.width,
-                 linestyle=self.style)
-        plt.text(self.x[0], text_y,
+        plt.plot(self.x, self.y, self.color, linewidth=self.width, linestyle=self.style)
+        plt.text(0.05 * len(self.y), k * 0.1 * (max_y - min_y),
                  self.text, color=self.color)
 
         if fig is None:
@@ -47,28 +33,30 @@ class Line:
 
 class Function(Line):
 
-    def __init__(self, x, func, line_width=1, line_color=None, text='', style='-'):
+    def __init__(self, x, func, line_width=1, line_color='black', text='', style='-'):
         y = [func(i) for i in x]
         super().__init__(x, y, line_width, line_color, text, style)
 
 
 class Constant(Line):
 
-    def __init__(self, x, c, line_width=1, line_color=None, text='', style='-'):
+    def __init__(self, x, c, line_width=1, line_color='black', text='', style='-'):
         x = [x[0], x[len(x) - 1]]
         y = [c] * len(x)
         super().__init__(x, y, line_width, line_color, text, style)
 
 
-def plot_lines(lines, seps=None, grid_flag=True, log=False, title=''):
+def plot_lines(lines, seps=None, grid_flag=True):
     fig = plt.figure()
     plt.grid(grid_flag)
-    plt.title(title)
-    plt.ylabel('y')
-    plt.xlabel('x')
+    plt.ylabel("y")
+    plt.xlabel("x")
     max_y = []
     min_y = []
+    count = 0
     for line in lines:
+        count += 1
+        line.plot(fig=fig, k=count)
         temp = line.y_range()
         min_y.append(temp[0])
         max_y.append(temp[1])
@@ -76,17 +64,7 @@ def plot_lines(lines, seps=None, grid_flag=True, log=False, title=''):
     min_y = np.amin(min_y)
     max_y = np.amax(max_y)
 
-    count = 1
-    y_range = max_y - min_y
-    for line in lines:
-        line.plot(fig=fig, text_y=0.5 * y_range * count / len(lines))
-        count += 1
-
-    if log:
-        plt.yscale('log')
-
-    if seps is not None:
-        for s in seps:
-            plt.plot([s - 0.001, s + 0.001], [min_y, max_y], 'r', linewidth=0.5)
+    for s in seps:
+        plt.plot([s - 0.001, s + 0.001], [min_y, max_y], 'r', linewidth=0.5)
 
     plt.show()
